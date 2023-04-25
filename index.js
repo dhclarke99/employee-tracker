f6b2c1655309cf5d398b51271808744b2de6bde4
dhclarke99 <dhclarke99@gmail.com>
2023-04-24 21:25:03 -0400

    created sql connection to index.js file

--- index.js
+++ index.js
@@ -1,4 +1,13 @@
 const inquirer = require('inquirer');
+const mysql = require('mysql2');
+// Create a MySQL connection
+const connect = mysql.createConnection({
+    host: '127.0.0.1',
+    port: 3306,
+    user: 'root',
+    password: 'tomnjerry',
+    database: 'company_db'
+  });
 
 
 const questions = () => { 
